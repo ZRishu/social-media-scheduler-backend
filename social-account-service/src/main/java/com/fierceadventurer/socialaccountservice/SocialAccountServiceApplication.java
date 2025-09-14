59776eb52486d0459ab729948f2b113b7cff2d12
fierce-adventurer <tripathiayush589@gmail.com>
2025-09-15 01:37:09 +0530

    started working on social account service this service connects the social account to the other two services. created the entities and enums and connected the database

---
+++ social-account-service/src/main/java/com/fierceadventurer/socialaccountservice/SocialAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fierceadventurer.socialaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialAccountServiceApplication.class, args);
+    }
+
+}
