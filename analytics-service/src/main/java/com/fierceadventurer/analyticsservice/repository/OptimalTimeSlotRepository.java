93dd0ab7214870f621db02327f8a85c8f38c3e34
fierce-adventurer <tripathiayush589@gmail.com>
2025-10-28 03:10:59 +0530

    created repository package for analytics service for service logic.

---
+++ analytics-service/src/main/java/com/fierceadventurer/analyticsservice/repository/OptimalTimeSlotRepository.java
@@ -0,0 +1,13 @@
+package com.fierceadventurer.analyticsservice.repository;
+
+import com.fierceadventurer.analyticsservice.entity.OptimalTimeSlot;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface OptimalTimeSlotRepository extends JpaRepository<OptimalTimeSlot, UUID> {
+    List<OptimalTimeSlot> findBySocialAccountIdOrderByEngagementScoreDesc(UUID socialAccountId);
+}
