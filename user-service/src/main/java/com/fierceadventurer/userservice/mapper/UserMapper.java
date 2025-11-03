e6f78e623eafbb943771d9c9568355e21ae94fc2
fierce-adventurer <tripathiayush589@gmail.com>
2025-11-04 01:59:22 +0530

    created mapper class for user service for converting dto top entity and vice versa

---
+++ user-service/src/main/java/com/fierceadventurer/userservice/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.fierceadventurer.userservice.mapper;
+
+import com.fierceadventurer.userservice.dto.UserResponseDto;
+import com.fierceadventurer.userservice.entity.User;
+import com.fierceadventurer.userservice.events.KeyClockUserCreatedEvent;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "id", source = "userId")
+    @Mapping(target = "displayName", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "createdAt" , ignore = true)
+    @Mapping(target = "updatedAt",  ignore = true)
+    User toEntity(KeyClockUserCreatedEvent event);
+
+    UserResponseDto toDto(User user);
+}
