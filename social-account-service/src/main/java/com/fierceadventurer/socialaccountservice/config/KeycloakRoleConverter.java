cddd664d36c473fc6f348b243418e1b93c965401
fierce-adventurer <tripathiayush589@gmail.com>
2025-11-05 04:03:44 +0530

    added security using keyCloak in social account service.

---
+++ social-account-service/src/main/java/com/fierceadventurer/socialaccountservice/config/KeycloakRoleConverter.java
@@ -0,0 +1,34 @@
+package com.fierceadventurer.socialaccountservice.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+    private static final String ROLE_PREFIX = "ROLE_";
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaimAsMap("realm_access");
+
+        if(realmAccess == null || realmAccess.isEmpty()) {
+            return List.of();
+        }
+
+        Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+
+        if(roles == null) {
+            return List.of();
+        }
+
+        return roles.stream()
+                .map(roleName -> ROLE_PREFIX + roleName.toUpperCase())
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ social-account-service/src/main/java/com/fierceadventurer/socialaccountservice/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.fierceadventurer.socialaccountservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/health",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/v1/accounts/**").authenticated()
+                        .requestMatchers("api/v1/internal/**").authenticated()
+                        .anyRequest().denyAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(new KeycloakJwtConverter()))
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+
+
+    }
+}
