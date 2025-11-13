3332a4d3b2ff9f100fc585f98c0bf995685fe6cf
fierce-adventurer <tripathiayush589@gmail.com>
2025-11-13 17:57:12 +0530

    Added exception handling classes to LinkedInApi.

---
+++ social-account-service/src/main/java/com/fierceadventurer/socialaccountservice/exception/LinkedInAuthenticationException.java
@@ -0,0 +1,7 @@
+package com.fierceadventurer.socialaccountservice.exception;
+
+public class LinkedInAuthenticationException extends RuntimeException {
+    public LinkedInAuthenticationException(String message) {
+        super(message);
+    }
+}
---
+++ social-account-service/src/main/java/com/fierceadventurer/socialaccountservice/exception/LinkedInServiceException.java
@@ -0,0 +1,7 @@
+package com.fierceadventurer.socialaccountservice.exception;
+
+public class LinkedInServiceException extends RuntimeException {
+    public LinkedInServiceException(String message) {
+        super(message);
+    }
+}
