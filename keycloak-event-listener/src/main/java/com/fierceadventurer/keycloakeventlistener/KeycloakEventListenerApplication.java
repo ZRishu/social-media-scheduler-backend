b210a3b9dd17cdbe8eff394782fc4e7c1f1e9b32
fierce-adventurer <tripathiayush589@gmail.com>
2025-11-23 23:46:28 +0530

    initiated keycloak event listener module for the .jar file

---
+++ keycloak-event-listener/src/main/java/com/fierceadventurer/keycloakeventlistener/KeycloakEventListenerApplication.java
@@ -0,0 +1,13 @@
+package com.fierceadventurer.keycloakeventlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakEventListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakEventListenerApplication.class, args);
+    }
+
+}
---
+++ keycloak-event-listener/src/test/java/com/fierceadventurer/keycloakeventlistener/KeycloakEventListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fierceadventurer.keycloakeventlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakEventListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
