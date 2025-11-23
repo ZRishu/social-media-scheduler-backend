188881e6cae000295e89ae66bf882af014110a43
fierce-adventurer <tripathiayush589@gmail.com>
2025-11-24 03:10:36 +0530

    fixed bug in my post service AnalyticsClient.java

--- post-service/src/main/java/com/fierceadventurer/postservice/client/AnalyticsClient.java
+++ post-service/src/main/java/com/fierceadventurer/postservice/client/AnalyticsClient.java
@@ -1,5 +1,6 @@
 package com.fierceadventurer.postservice.client;
 
+import com.fierceadventurer.postservice.config.FeignConfig;
 import com.fierceadventurer.postservice.dto.NextBestTimeResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
-@FeignClient(name = "analytics-service", url = "${clients.analytics-service.url}",configuration = FeignClient.class)
+@FeignClient(name = "analytics-service", url = "${clients.analytics-service.url}",configuration = FeignConfig.class)
 public interface AnalyticsClient {
     @GetMapping("/api/v1/analytics/{socialAccountId}/next-best-time")
     NextBestTimeResponseDto getNextBestTime(@PathVariable("socialAccountId") UUID socialAccountId);
