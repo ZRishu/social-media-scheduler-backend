43b5dd040b96220d7c5cd9abe9df1fabbc3cf871
fierce-adventurer <tripathiayush589@gmail.com>
2025-11-27 16:15:57 +0530

    implemented minio s3 in my media service

---
+++ media-storage-service/src/main/java/com/fierceadventurer/mediastorageservice/dto/MediaResourceDto.java
@@ -0,0 +1,14 @@
+package com.fierceadventurer.mediastorageservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MediaResourceDto {
+    private String fileName;
+    private String contentType;
+    private byte[] data;
+}
