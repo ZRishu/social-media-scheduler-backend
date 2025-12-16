d63a3b8b333514214a2f91424888fc2ecc44b62c
fierce-adventurer <tripathiayush589@gmail.com>
2025-12-17 00:37:45 +0530

    Removed unneccessary annonation from HistoricalPost.java

--- analytics-service/src/main/java/com/fierceadventurer/analyticsservice/dto/HistoricalPost.java
+++ analytics-service/src/main/java/com/fierceadventurer/analyticsservice/dto/HistoricalPost.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
 public class HistoricalPost {
     private LocalDateTime createdAt;
     private int engagementCount;
