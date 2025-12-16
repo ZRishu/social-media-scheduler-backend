742743d9e9cf558e77ed811c782e1f952caf9a8d
fierce-adventurer <tripathiayush589@gmail.com>
2025-12-17 00:55:41 +0530

    fixed a minor bug addinga all args constructor annotation for HistoricalPost.java

--- analytics-service/src/main/java/com/fierceadventurer/analyticsservice/dto/HistoricalPost.java
+++ analytics-service/src/main/java/com/fierceadventurer/analyticsservice/dto/HistoricalPost.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class HistoricalPost {
     private LocalDateTime createdAt;
     private int engagementCount;
